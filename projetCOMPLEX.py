01907e09886a03f492320aaefa53576572d3a2be
Natacha Rivière <riviere.natachamarie@gmail.com>
2023-10-17 00:04:36 +0200

    branch and bound fonctionnel

--- projetCOMPLEX.py
+++ projetCOMPLEX.py
@@ -127,13 +127,15 @@ generatedGraph = createGraph(5, 0.5)
 
 def algo_couplage(graph):
     C = set()
+    M=0
     for sommet in graph:
         for arrete in graph[sommet]:
             if sommet not in C :
                 if arrete not in C :
                     C.add(sommet)
                     C.add(arrete)
-    return C
+                    M+=1
+    return [C, M]
             
 def algo_glouton(graph):
     graph2 = deepCopy(graph)
@@ -237,7 +239,7 @@ def etudierSommet(graph, sommet):
         g = etudierSommet(myGraph, selectedArete[0])  
         d = etudierSommet(myGraph, selectedArete[1])
         #Si la solution de gauche est la meilleure, on la renvoie, l'autre sinon
-        if min(g[0], d[0]) == g :
+        if min(g[0], d[0]) == g[0] :
             g[1].append(sommet)
             return [g[0] + 1, g[1]]
         else:
@@ -252,11 +254,11 @@ def etudierSommet(graph, sommet):
 
 #question 2
 
+UB = math.inf
+
 def branchementCouplage(graph):
     selectedArete = 0
-    #Calcul des bornes sup et inf
-    borne_min = maxB(graph)
-    borne_sup = algo_couplage(graph)
+    
     # On selectionne une arête 
     for sommet in graph.keys():
         if len(graph[sommet]) != 0:
@@ -264,11 +266,9 @@ def branchementCouplage(graph):
             break
     if selectedArete :
         #Il y a des arêtes, on crée un noeud avec le sommet gauche en moins et un autre avec le sommet droit en moins
-        g = etudierSommetCouplage(graph, selectedArete[0], UB) 
-        if (g[2]+1 <= UB) :
-            UB = g[2] + 1
-        d = etudierSommetCouplage(graph, selectedArete[1], UB)
-
+        g = etudierSommetCouplage(graph, selectedArete[0], 1) 
+        d = etudierSommetCouplage(graph, selectedArete[1], 1)
+        # print("g: ", g, "d: ", d)
         #Si la solution de gauche est la meilleure, on la renvoie, l'autre sinon
         if min(g[0], d[0]) == g[0] :
             return g
@@ -277,64 +277,76 @@ def branchementCouplage(graph):
     else:
         # Il n'y a pas d'arêtes à étudier, la couveture es de taille 0
         return 0
+
     
-    
-def etudierSommetCouplage(graph, sommet, UB):
+def etudierSommetCouplage(graph, sommet, profondeur):
     myGraph = deleteOne(graph, sommet) # On supprime le sommet donné
     selectedArete = 0
-    #Calcul des bornes sup et bornes inf
-    borne_min = maxB(myGraph)
-    borne_sup = algo_couplage(myGraph)
-
-    if (borne_min+1 >= UB) :
-        return [math.inf, []] # La branche n'est pas intéressante
-    
-    if (borne_sup+1 <= UB) :
-        UB = borne_sup+1 #On a trouvé une meilleure solution
-
     # On selectionne une arête 
     for sommetEtudie in myGraph.keys():
         if len(myGraph[sommetEtudie]) != 0:
             selectedArete = [sommetEtudie, min(myGraph[sommetEtudie])]
             break
     if selectedArete :
+        #Calcul des bornes sup et bornes inf
+        couplage = algo_couplage(myGraph)
+        borne_min = maxB(myGraph, couplage)
+        global UB
+        if (borne_min+profondeur > UB) : 
+            return [math.inf, []] # La branche n'est pas intéressante
+        
+        borne_sup = len(couplage[0])
+
+        if (borne_sup+profondeur <= UB) :
+            UB = borne_sup+profondeur #On a trouvé une meilleure solution
         #Il y a des arêtes, on crée un noeud avec le sommet gauche en moins et un autre avec le sommet droit en moins
-        g = etudierSommetCouplage(myGraph, selectedArete[0], UB-1)
-        if (g[2]+1 <= UB) :
-            UB = g[2] + 1
-        d = etudierSommetCouplage(myGraph, selectedArete[1], UB-1)
-        if (d[2]+1 <= UB) :
-            UB = d[2] + 1
+        g = etudierSommetCouplage(myGraph, selectedArete[0], profondeur+1)
+        d = etudierSommetCouplage(myGraph, selectedArete[1], profondeur+1)
+
+
         #Si la solution de gauche est la meilleure, on la renvoie, l'autre sinon
-        if min(g[0], d[0]) == g :
+        if min(g[0], d[0]) == g[0] :
             g[1].append(sommet)
-            return [g[0] + 1, g[1], UB]
+            return [g[0] + 1, g[1]]
         else:
             d[1].append(sommet)
-            return [d[0] + 1, d[1], UB]
+
+            return [d[0] + 1, d[1]]
     else:
         # Il n'y a plus d'arêtes à étudier, la couveture es de taille 1, le sommet retiré
-        return [1, [sommet], UB]
+        return [1, [sommet]]
 
-def maxB(graph):
-    graph2 = deepCopy(graph)
-    couplage = algo_couplage(graph2)
-    print("couplage : ", couplage)
-    
-    b2 = len(couplage)
+def maxB(graph, couplage):
+    graph2 = deepCopy(graph)    
+    b2 = couplage[1]
     
     #décompte arêtes 
-    delta = degreMaximal(graph2)
-    m = 0
-    for sommet in graph2:
-        m+= len(graph2[sommet])
-        for arrete in list(graph2[sommet]):
-            graph2[arrete].remove(sommet)
-        del graph2[sommet]
-
+    deg = degreSommets(graph2)
+    delta = max(deg)
+    m = sum(deg) / 2
     b1 = math.ceil(m/delta)
+
     n = len(graph)
     b3 = ((2*n)-1-math.sqrt(((2*n-1)**2)-8*m))/2
     return max([b1, b2, b3])
 
-# print("solution : ", branchementCouplage(graph))
+
+# graph = {0: {2}, 1: {3, 4}, 2: {0}, 3: {1, 4}, 4: {1, 3}}
+graph = createGraph(28, 0.5)
+start = time.time()
+a = branchementCouplage(graph)
+end = time.time()
+
+elapsed = end - start
+
+print(f'Temps d\'execution algo pas naif : {elapsed:.5}s')
+print("solution pas naive :", a)
+
+start = time.time()
+a = branchement(graph)
+end = time.time()
+
+elapsed = end - start
+
+print(f'Temps d\'execution algo naif : {elapsed:.5}s')
+print("solution naive :", a)
